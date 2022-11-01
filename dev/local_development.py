8eb0fef246186b58dd5c0e88a4502ff879637a81
Jakob Lund Johannessen <jakob-lj@users.noreply.github.com>
2022-11-01 15:48:33 +0100

    chore: update host and port

--- dev/local_development.py
+++ dev/local_development.py
@@ -1,14 +1,12 @@
 from flask import Flask, request
 from flask_cors import CORS
 import sys
-import json
 
 sys.path.append("../src")
 
 # Must be imported after module have been appended
+from handle_next_move_call import handle_next_move
 from move import Move
-import tic_tac
-
 
 api = Flask(__name__)
 CORS(api)
@@ -16,22 +14,14 @@ CORS(api)
 
 @api.route("/", methods=["POST"])
 def handleTicTac():
-
     board = request.json['board']
-    _legal_moves = request.json['legalMoves']
+    legal_moves = [Move(x=it['x'], y=it['y'], player=it['player'])
+                   for it in request.json['legalMoves']]
 
-    if (len(_legal_moves) != 0):
-
-        legal_moves = [Move(x=it['x'], y=it['y'], player=it['player'])
-                    for it in _legal_moves]
-
-        current_player = request.json['currentPlayer']
-        history = request.json['history']
-
-        return json.dumps({"move": tic_tac.next_move(board, legal_moves, current_player, history).serialize()})
-    else:
-        return json.dumps({"error": "Did not find any legal moves"})
+    current_player = request.json['currentPlayer']
+    history = request.json['history']
+    return {"move": handle_next_move(board, legal_moves, current_player, history)}
 
 
 if __name__ == '__main__':
-    api.run()
+    api.run(host="localhost", port=8000)
