36f11b69ffabcc815457ed855c8dc5f8574cd3bc
Jakob Lund Johannessen <jakob-lj@users.noreply.github.com>
2022-11-01 11:21:22 +0100

    chore: add some error handling

--- src/main.py
+++ src/main.py
@@ -1,5 +1,30 @@
 
-def handler(event, context):
-    print(event)
+from move import Move
+import tic_tac
 
-    return {"statusCode": 200, "body": {"test": "data"}}
+def handler(event, context):
+    
+    body = event['body']
+    response = {}
+    if (len(legal_moves) != 0):
+
+        board = body['board']
+
+        legal_moves = [Move(x=it['x'], y=it['y'], player=it['player'])
+                    for it in body['legalMoves']]
+
+        current_player = body['currentPlayer']
+        history = body['history']
+
+        try: 
+            move = tic_tac.next_move(board, legal_moves, current_player, history)
+            response = {"move": move.serialize()}
+        except:
+            reponse = {"error": "Applicaiton catched an error"}
+
+    else:
+        response = {
+            "error": "Did not find any legal moves"
+        }
+
+    return {"statusCode": 200, "body": response}
