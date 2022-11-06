ec740e98d20c1d8e5492d9c6fd8884b7b23e67b6
Jakob Lund Johannessen <jakob-lj@users.noreply.github.com>
2022-11-06 14:33:33 +0100

    chore: setup config to take more members

--- src/config.py
+++ src/config.py
@@ -2,5 +2,6 @@
 def get_config():
     return {
         "groupName": "group-<group-number>",
-        "groupNumber": "<group-number>"
+        "groupNumber": "<group-number>",
+        "groupMembers": ["Member 1", "Member 2"]
     }
